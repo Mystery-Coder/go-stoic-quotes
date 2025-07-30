499eaccd7d5d3094e9d53c9306876d3789c01010
MysteryCoder <srikar0811@gmail.com>
2025-07-30 08:39:29 +0530

    Demo site

--- demo/script.js
+++ demo/script.js
@@ -1,9 +1,35 @@
-let quote = {
-	author: "Seneca",
-	quote: "All cruelty springs from weakness.",
-};
+function getQuoteSVG(theme = "default") {
+	const URL = `https://go-stoic-quotes-production.up.railway.app/stoic-quote-svg?theme=${encodeURIComponent(
+		theme
+	)}`;
 
-$("#btn").click(function (e) {
-	e.preventDefault();
-	console.log("sdsd");
+	// Show loading text
+	$("#quote-svg").html(
+		`<p class="text-gray-500 italic">Loading quote...</p>`
+	);
+
+	setTimeout(() => {
+		$("#quote-svg").html(`
+			<object 
+				data="${URL}" 
+				type="image/svg+xml" 
+				width="300" 
+				height="300" 
+				class="mx-auto"
+			></object>
+		`);
+	}, 200);
+
+	// Update output preview
+	$("#url").text(URL);
+	$("#readme").text(`![Stoic Quotes](${URL})`);
+}
+
+$(document).ready(function () {
+	getQuoteSVG();
+
+	$("#theme").on("change", function () {
+		const selectedTheme = $(this).val();
+		getQuoteSVG(selectedTheme);
+	});
 });
